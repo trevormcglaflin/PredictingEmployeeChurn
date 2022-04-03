f7ae1322dfb53ba2a0c09a051181a05f6733209f
Trevor McGlaflin <trevor.mcglaflin@uvm.edu>
2022-04-03 15:25:42 -0400

    made python file

---
+++ ChurnAnalysis.py
@@ -0,0 +1,18 @@
+# Trevor McGlaflin and Sam Noel
+# April 3, 2022
+# Predicting Employee Churn
+# CS/STAT 287
+
+def main():
+    #TODO: read in data, store in dataframe
+    #TODO: clean data, I think it's very clean data already so we don't need to remove any rows or anything
+    #TODO: remove unneccesary/redudant columns
+    #TODO: add any calculated fields
+    #TODO: plot some basic relationships between columns, and do some exploratory data analysis
+    #TODO: do a linear multiple regression on the data to predict attrition
+    #TODO: try some other techniques to predict attrition
+    #TODO: compare the accuracy of the ML algorithms
+    pass
+
+if __name__ == '__main__':
+    main()
